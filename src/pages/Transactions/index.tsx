e2536215e5a20dc31ba0324414260e2516adc3b1
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-08-08 21:49:11 -0300

    Componente Summary criado

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
 
 import { TransactionsContainer } from './styles'
 
@@ -6,6 +7,7 @@ export function Transactions() {
   return (
     <TransactionsContainer>
       <Header />
+      <Summary />
     </TransactionsContainer>
   )
 }
