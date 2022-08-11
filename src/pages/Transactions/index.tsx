89318a73ee4d2d0b2be049fce423fcf022b4417f
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-08-10 22:35:16 -0300

    Visual da tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,13 +1,41 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 
-import { TransactionsContainer } from './styles'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsListContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <TransactionsContainer>
       <Header />
       <Summary />
+
+      <TransactionsListContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="positive">R$ 1200,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>10/08/2004</td>
+            </tr>
+            <tr>
+              <td width="40%">Hambúrger</td>
+              <td>
+                <PriceHighlight variant="negative">- R$ 14,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/08/2004</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsListContainer>
     </TransactionsContainer>
   )
 }
